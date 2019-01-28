f18d9493d8f71ac1dd309af541891aedd144ca25
Evan Guirino <etg624@gmail.com>
2019-01-28 09:22:04 -0800

    skeleton reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,19 @@
+import { RESTART_GAME, MAKE_GUESS, GENERATE_AURAL_UPDATE } from '../actions';
+
+const initialState = {
+  guesses: [],
+  feedback: 'Make your guess!',
+  auralStatus: '',
+  correctAnswer: Math.floor(Math.random() * 100) + 1
+};
+
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    case RESTART_GAME:
+    case MAKE_GUESS:
+    case GENERATE_AURAL_UPDATE:
+      return state;
+    default:
+      return state;
+  }
+}
