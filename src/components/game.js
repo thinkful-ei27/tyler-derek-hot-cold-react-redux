601b0cd8be3351368754e7431f694beda84a6e5c
Tyler Crabb <tydacrabb@gmail.com>
2019-01-29 09:23:45 -0800

    works

--- src/components/game.js
+++ src/components/game.js
@@ -1,32 +1,18 @@
 import React from 'react';
-import {connect} from 'react-redux';
 import Header from './header';
 import GuessSection from './guess-section';
 import StatusSection from './status-section';
 import InfoSection from './info-section';
 
-  class Game extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      guesses: [],
-      feedback: 'Make your guess!',
-      auralStatus: '',
-      correctAnswer: Math.floor(Math.random() * 100) + 1
-    };
-  }
-
+export default class Game extends React.Component {
+  
   render() {
-    const { guesses, auralStatus } = this.state;
-
     return (
       <div>
         <Header/>
         <main role="main">
           <GuessSection />
-          <StatusSection guesses={guesses} 
-            auralStatus={auralStatus}
-          />
+          <StatusSection/>
           <InfoSection />
         </main>
       </div>
@@ -34,5 +20,3 @@ import InfoSection from './info-section';
   }
 }
 
-
-export default connect()(Game)
--- src/components/status-section.js
+++ src/components/status-section.js
@@ -1,18 +1,25 @@
 import React from 'react';
-
+import {connect} from 'react-redux'
 import GuessList from './guess-list';
 import GuessCount from './guess-count';
 import AuralStatus from './aural-status';
 
-export default function StatusSection(props) {
-  const { guesses, auralStatus } = props;
-  const guessCount = guesses.length;
-
+function StatusSection(props) {
   return (
     <section aria-labelledby="guessCount" aria-describedby="guessList">
-      <GuessCount guessCount={guessCount} />
-      <GuessList guesses={guesses} />
-      <AuralStatus auralStatus={auralStatus} />
+      <GuessCount guessCount={props.guessCount} />
+      <GuessList guesses={props.guesses} />
+      <AuralStatus auralStatus={props.auralStatus} />
     </section>
   );
 }
+
+const mapStateToProps = state => {
+  return {
+    guesses: state.guesses,
+    guessCount:state.guesses.length,
+    auralStatus: state.auralStatus,
+  }
+
+}
+export default connect(mapStateToProps)(StatusSection)
